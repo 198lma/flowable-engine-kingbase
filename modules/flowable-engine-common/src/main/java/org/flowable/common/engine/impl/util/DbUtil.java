0aa429dc91bbd08df8b1f9c983be48339e4e3190
zjj <2013359110@qq.com>
2025-09-28 16:47:59 +0800

    修改flowable源码适配kingbase-oracle模式

--- modules/flowable-engine-common/src/main/java/org/flowable/common/engine/impl/util/DbUtil.java
+++ modules/flowable-engine-common/src/main/java/org/flowable/common/engine/impl/util/DbUtil.java
@@ -41,6 +41,7 @@ public class DbUtil {
     public static final String DATABASE_TYPE_MSSQL = "mssql";
     public static final String DATABASE_TYPE_DB2 = "db2";
     public static final String DATABASE_TYPE_COCKROACHDB = "cockroachdb";
+    public static final String DATABASE_TYPE_KINGBASE = "kingbase";
 
     public static Properties getDefaultDatabaseTypeMappings() {
         Properties databaseTypeMappings = new Properties();
@@ -73,6 +74,7 @@ public class DbUtil {
         databaseTypeMappings.setProperty("DB2/PTX", DATABASE_TYPE_DB2);
         databaseTypeMappings.setProperty("DB2/2", DATABASE_TYPE_DB2);
         databaseTypeMappings.setProperty("DB2 UDB AS400", DATABASE_TYPE_DB2);
+        databaseTypeMappings.setProperty("KingbaseES", DATABASE_TYPE_KINGBASE);
         databaseTypeMappings.setProperty(PRODUCT_NAME_CRDB, DATABASE_TYPE_COCKROACHDB);
         return databaseTypeMappings;
     }
